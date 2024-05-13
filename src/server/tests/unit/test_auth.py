bcaffae0c9d8c19b0102f04138632e3feacb09b6
Aech27 <hembergt@gmail.com>
2024-05-13 12:07:25 +0200

    cool beans

--- src/server/tests/unit/test_auth.py
+++ src/server/tests/unit/test_auth.py
@@ -2,14 +2,29 @@ import pytest
 from sql.databaseMGR import DatabaseManager
 from sql.user import User
 
+# Define DatabaseManager to execute sql queries
+dbmgr = DatabaseManager()
+
 def test_user_signup():
+    """
+    Test user signup using the User class
+    """
     pass
 
 def test_user_signup_errors():
+    """
+    Test errors on user signup using the User class
+    """
     pass
 
 def test_user_login():
+    """
+    Test user login using the User class
+    """
     pass
 
 def test_user_login_errors():
+    """
+    Test errors on user login using the User class
+    """
     pass
--- src/server/tests/unit/test_benchmarks.py
+++ src/server/tests/unit/test_benchmarks.py
@@ -1,7 +1,15 @@
 import pytest
+from sql.databaseMGR import DatabaseManager
+from sql.benchmark import Benchmark
 
 def test_score_save():
+    """
+    Test score saving using the Benchmark class
+    """
     pass
 
 def test_score_get():
+    """
+    Test get score using the Benchmark class
+    """
     pass
